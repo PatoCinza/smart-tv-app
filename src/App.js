407655c9f78bd1c2472d795f97825c6c2dcedcab
Gabriel Luis <gabriel.luis.cinza@gmail.com>
2020-11-05 10:07:39 -0300

    render things on html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import jikanjs from 'jikanjs'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      animes: []
+    }
+  }
+
+  componentDidMount () {
+    jikanjs.loadSeasonLater()
+      .then(res => {
+        const animes = res.anime.map(anime => anime)
+        this.setState({ animes })
+      })
+      .catch(console.error)
+  }
+
+  tmpl ({ image_url = '', title = '', mal_id = '', type = '' }) {
+    return (
+      <div className="poster-wrapper" id={mal_id} key={mal_id}>
+        <figure>
+          <span>{type}</span>
+          <img src={image_url} alt={title} />
+          <p>{title}</p>
+        </figure>
+      </div>
+    )
+  }
+
   render () {
-    return <h1>Hello world</h1>
+    let animes = this.state.animes
+
+    return animes.filter(anime => anime.type === 'TV').map(this.tmpl)
   }
 }
 
